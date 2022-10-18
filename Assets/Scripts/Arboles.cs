928aee6d92b5427614e0a47fb67b2452d3ee69ff
beluvinse <83471976+beluvinse@users.noreply.github.com>
2022-10-18 19:54:53 -0300

    arboles

---
+++ Assets/Scripts/Arboles.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Arboles : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    private void Start()
+    {
+        Renderer = Renderer ?? GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        if ((Camera.main == null) || (Renderer == null))
+        {
+            return;
+        }
+
+        Bounds adjustedBounds = Renderer.bounds;
+        adjustedBounds.center = Camera.main.transform.position + (Camera.main.transform.forward * (Camera.main.farClipPlane - Camera.main.nearClipPlane) * 0.5f);
+        adjustedBounds.extents = new Vector3(0.1f, 0.1f, 0.1f);
+
+        Renderer.bounds = adjustedBounds;
+    }
+}
